f6966c68df9d024797c42af8687357c042441d01
Erik Roznbeker <erik.roznbeker@superius.hr>
2015-08-07 12:59:45 +0200

    autodetect id item has children so parameter 'hasChilds' is now optional

--- example/app.js
+++ example/app.js
@@ -10,21 +10,21 @@ function treeBoxController($scope, $rootScope, $http, $log, $window) {
     /* Example data */
     $scope.treeBoxData = [
 
-        {id: 1, value: "Cars", hasChilds: true},
-        {id: 4, value: "Boat1", hasChilds: false},
-        {id: 4, value: "Boat2", hasChilds: false},
-        {id: 4, value: "Boat3", hasChilds: false},
-        {id: 4, value: "Boat4", hasChilds: false},
-        {id: 4, value: "Boat5", hasChilds: false},
-        {id: 4, value: "Boat6", hasChilds: false},
+        {id: 1, value: "Cars"},
+        {id: 4, value: "Boat1"},
+        {id: 4, value: "Boat2"},
+        {id: 4, value: "Boat3"},
+        {id: 4, value: "Boat4"},
+        {id: 4, value: "Boat5"},
+        {id: 4, value: "Boat6"},
 
-        {id: 15, value: "Audi", parent: 1, hasChilds: true},
-        {id: 16, value: "BMW", parent: 1, hasChilds: false},
-        {id: 17, value: "Mercedes", parent: 1, hasChilds: false},
+        {id: 15, value: "Audi", parent: 1},
+        {id: 16, value: "BMW", parent: 1},
+        {id: 17, value: "Mercedes", parent: 1},
 
-        {id: 21, value: "A3", parent: 15, hasChilds: false},
-        {id: 22, value: "A6", parent: 15, hasChilds: false},
-        {id: 23, value: "R8", parent: 15, hasChilds: false},
+        {id: 21, value: "A3", parent: 15},
+        {id: 22, value: "A6", parent: 15},
+        {id: 23, value: "R8", parent: 15},
 
     ];
 
